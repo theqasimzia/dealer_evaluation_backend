339a30ea6beb76f4393a52e61c3cc466bab1a649
Muhammad Qasim Zia <mqasim.zia.92@gmail.com>
2024-07-15 00:47:57 +0500

    first commit

--- dealer_details/server.js
+++ dealer_details/server.js
@@ -1,52 +1,48 @@
 const express = require("express");
-var cors = require('cors')
+var cors = require('cors');
 const app = express();
 app.use(cors());
 
 const dealerslist = require("./utils/dealers.json");
 
-
-
 app.get("/price/:dealer/:product", function (request, response) {
   let dealers = dealerslist.dealers;
   let req_dealer = request.params.dealer;
   let req_product = request.params.product;
-  let resp = false;
+  let message = "The product is not available with this dealer";
+  
   dealers.forEach((dealer) => {
-    if(dealer.Dealer === req_dealer) {
-      if(dealer.products[req_product]) {
-        response.send({"message":req_product+" costs "+ dealer.products[req_product]+ " at "+req_dealer});
-        resp = true;
+    if (dealer.Dealer === req_dealer) {
+      if (dealer.products[req_product]) {
+        message = req_product + " costs " + dealer.products[req_product] + " at " + req_dealer;
       } else {
-        response.send({"message":req_product+" is not available with "+ req_dealer});
-        resp = true;
+        message = req_product + " is not available with " + req_dealer;
       }
     }
   });
-  if(!resp) {
-    response.send({"message":"The product is not available with this dealer"})
-  }
-
-})
+  
+  response.send({"message": message});
+});
 
 app.get("/allprice/:product", function (request, response) {
   let dealers = dealerslist.dealers;
   let req_product = request.params.product;
   let priceslist = [];
+  
   dealers.forEach((dealer) => {
-    if(dealer.products[req_product]) {
-      priceslist.push({key:dealer.Dealer,value:dealer.products[req_product]});
+    if (dealer.products[req_product]) {
+      priceslist.push({key: dealer.Dealer, value: dealer.products[req_product]});
     }
   });
-  if(priceslist.length > 0) {
-    response.send({"prices":priceslist})
+
+  if (priceslist.length > 0) {
+    response.send({"prices": priceslist});
   } else {
-    response.send({"message":"The product is not available with this dealer"})
+    response.send({"message": "The product is not available with this dealer"});
   }
+});
 
-})
-
-let port = process.env.PORT || 8080
+let port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log("To view your app, open this link in your browser: http://localhost:" + port);
-})
+});
